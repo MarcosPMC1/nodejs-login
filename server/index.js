ebb021ba7a793181f3d63260ac75500af28b7c27
Marcos <marcospmc@outlook.com>
2022-04-23 02:22:53 -0300

    implementação de verificação de email em registro e sistema de login backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require("cors");
+let bcrypt = require("bcrypt");
 
 const db = mysql.createPool({
     host: 'localhost',
@@ -20,21 +21,54 @@ app.post("/register", (req, res) => {
     const { email } = req.body;
 
     let SQL = "INSERT INTO users ( user, password, email ) VALUES ( ?,?,? )";
+    let verifySQL = "SELECT * FROM users WHERE email = ?";
+    //verifica se o email ja esta cadastrado
+    db.query(verifySQL, [email], (err, result) =>{
+        console.log(result.length);
+        if(err){
+            res.send(err);
+        }
+        //caso nao
+        if(result.length == 0){
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(password, salt, function(err, hash) {
+                    //registra novo usuario
+                    db.query(SQL, [user, hash, email], (err, result) => {
+                        console.log(err);
+                        res.send({msg: "Usuario cadastrado com sucesso"});
+                        console.log("SUCESSO");
+                    });
+                });
+            }); 
+        } else {
+            res.send({msg: "Usuario com este EMAIL já cadastrado!"});
+            console.log("ERRO 2");
 
-    db.query(SQL, [user, password, email], (err, result) => {
-        console.log(err);
-    });
+        }
+    })
 });
 
-app.get("/getcards", (req, res) => {
-    let SQL = "SELECT * from games";
+app.post("/login", (req, res) => {
+    const { email } = req.body;
+    const { password } = req.body;
 
-    db.query(SQL, (err, result) => {
-        //caso tenha erro mostre senao apenas exibir resultado
-        if (err) console.log(err);
-        else res.send(result);
-    })
-})
+    db.query("SELECT * FROM users WHERE email = ?", [email], (err, result) => {
+        if(err){
+            req.send(err);
+        }
+        if(result.length > 0){
+            bcrypt.compare(password, result[0].password, (error, result) => {
+                if(result){
+                    res.send("Login efetuado com sucesso!");
+                } else {
+                    res.send("Senha incorreta!");
+                }
+            });
+        } else {
+            res.send({ msg: "Conta não encontrada!" });
+        }
+    });
+});
 
 app.listen(3001, () => {
     console.log('Servidor rodando');
